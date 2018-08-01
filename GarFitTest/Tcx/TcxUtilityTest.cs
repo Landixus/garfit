9326f21407d3ffb41a242527ac2f18dc908dc261
Hung Dinh <hung.dnc@gmail.com>
2018-08-01 20:53:21 +0700

    Add test project

---
+++ GarFitTest/Tcx/TcxUtilityTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using DateTime = Dynastream.Fit.DateTime;
+using SysDateTime = System.DateTime;
+using GarFit.TCX;
+
+namespace GarFitTest.Tcx
+{
+    [TestFixture()]
+    public class TcxUtilityTest
+    {
+        [Test()]
+		public void ToFitTime_Test()
+        {
+			string dateTimeString = "2017-11-27T12:16:30.000Z";
+			SysDateTime actual = new SysDateTime(2017, 11, 27, 12, 16, 30, DateTimeKind.Utc);
+
+			DateTime dateTime = TcxUtility.ToFitTime(dateTimeString);
+			Assert.AreEqual(dateTime.GetDateTime(), actual);
+        }
+    }
+}
