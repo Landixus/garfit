f1d6483269b18b28a71c41f5c3b594df72f9c72d
nhdinh <hung.dnc@gmail.com>
2018-08-02 08:59:29 +0700

    Remove bin & object folder. Update project

--- GarFitTest/Tcx/TcxUtilityTest.cs
+++ GarFitTest/Tcx/TcxUtilityTest.cs
@@ -10,7 +10,7 @@ namespace GarFitTest.Tcx
     public class TcxUtilityTest
     {
         [Test()]
-		public void ToFitTime_Test()
+		public void ToFitTime_Test_Pass()
         {
 			string dateTimeString = "2017-11-27T12:16:30.000Z";
 			SysDateTime actual = new SysDateTime(2017, 11, 27, 12, 16, 30, DateTimeKind.Utc);
@@ -18,5 +18,17 @@ namespace GarFitTest.Tcx
 			DateTime dateTime = TcxUtility.ToFitTime(dateTimeString);
 			Assert.AreEqual(dateTime.GetDateTime(), actual);
         }
+		
+		[Test()]
+		public void ToFitTime_Test_FailDueToConvertException() {
+			string dateTimeString = "";
+			DateTime dateTime = null;
+			try {
+				dateTime = TcxUtility.ToFitTime(dateTimeString);
+			} catch {
+				Assert.Fail();
+				Assert.AreEqual(dateTime, null);
+			}
+		}
     }
 }
